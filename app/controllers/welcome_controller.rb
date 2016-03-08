3e974d71ae21fbef4f00d50a7d31544df82c47de
jramorris <jamesramorris@gmail.com>
2016-03-07 23:23:39 -0800

    tweet shim + stylings

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,17 @@
+require 'uri'
+
 class WelcomeController < ApplicationController
   def index
-    @client = Twitter::REST::Client.new do |config|
+    client = Twitter::REST::Client.new do |config|
       config.consumer_key = "aVCresL99gsHG6McK7nNvOIFt"
       config.consumer_secret = "Ck4tm8qoAYp0zno66vrfKpytnmJlt3e7MhefLljMFSLg4oYXp9"
       config.access_token = "324123882-28coMhHnj3hE7KM0TVaGDFciAV9dyHwq9DXeyXif"
       config.access_token_secret = "M6IdCuBipF9GxEyLoqFZgp7rzZehynbwZSSFd4zNuJp0c"
     end
 
-    @alex = @client.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=alexmoskowitz')
-    @garry = @client.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=gioiacobucci')
+    @garry = client.get('https://api.twitter.com/1.1/users/show.json?screen_name=gioiacobucci')
+    @tokyo = client.get('https://api.twitter.com/1.1/users/show.json?screen_name=Tokyo_Art_Days')
+    @alex = client.get('https://api.twitter.com/1.1/users/show.json?screen_name=alexmoskowitz')
+    @james = client.get('https://api.twitter.com/1.1/users/show.json?screen_name=jamesrainmorris')
   end
 end
